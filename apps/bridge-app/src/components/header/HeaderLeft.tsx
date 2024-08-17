af33a41b2d24c7c3bc5388656f6c39f937fef0d4
dzung <dzung@caliber.build>
2024-08-17 15:56:26 +0700

    feat: rebrand

--- apps/bridge-app/src/components/header/HeaderLeft.tsx
+++ apps/bridge-app/src/components/header/HeaderLeft.tsx
@@ -1,11 +1,20 @@
+import { useTheme } from "@/providers/ThemeProvider";
+import logoNameDark from '@/assets/rise-name-dark.svg'
+import logoNameLight from '@/assets/rise-name.svg'
+
 export type HeaderLeftProps = {
   logo: string
 }
 
 export const HeaderLeft = ({ logo }: HeaderLeftProps) => {
+  const { theme } = useTheme();
+
   return (
     <div className="flex logo">
       <img src={logo} />
+      {theme == "dark" ?
+        <img className="p-4" src={logoNameLight} />
+      :<img className="p-4" src={logoNameDark} />}
     </div>
   )
 }
